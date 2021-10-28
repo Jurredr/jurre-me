d4db269352c9e3623155f51d0b5447ef169fef15
Jurredr <github@jurre.me>
2021-10-28 17:59:27 +0200

    Added custom font declarations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        freigeist: ['Freigeist'],
+        euclid: ['Euclid Circular B']
+      }
+    }
   },
   variants: {
     extend: {}
