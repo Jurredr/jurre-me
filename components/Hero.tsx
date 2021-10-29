b99fff9ed876c7aab7bed0717efffddf6d38d16c
Jurredr <github@jurre.me>
2021-10-29 09:41:33 +0200

    Added Hero component base

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="bg-yellow-300">
+      <p>JURRE</p>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Hero />
       <Footer />
     </div>
   )
