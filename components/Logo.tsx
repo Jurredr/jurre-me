4355637123c5a7cd1ee2d276eb201309b9d83807
Jurredr <github@jurre.me>
2021-10-29 10:26:31 +0200

    Color prop in Logo is now optional

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import Image from 'next/image'
 
 interface Props {
-  color: string
+  color?: string
 }
 
 const Logo: React.FC<Props> = (props) => {
   return (
     <div className="footer-logo flex items-baseline justify-center gap-2 cursor-default noselect">
       <Image
-        className={props.color}
+        className={props.color || 'text-black'}
         src="/icons/logo-icon.svg"
         alt=""
         width={27}
         height={28}
         draggable="false"
       />
-      <p className={props.color + ' font-medium text-3xl'}>Jurre.me</p>
+      <p className={(props.color || 'text-black') + ' font-medium text-3xl'}>
+        Jurre.me
+      </p>
     </div>
   )
 }
