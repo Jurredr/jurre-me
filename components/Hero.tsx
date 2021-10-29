2160ec6a971a1d648f22a42e36f88ba8df0b2594
Jurredr <github@jurre.me>
2021-10-29 10:27:48 +0200

    Added further styling to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Logo from './Logo'
 
 export default function Hero() {
   return (
-    <div className="bg-jurreyellow-dark flex justify-center font-freigeistwide font-black">
-      <p className="text-jurreyellow-light">JURRE</p>
+    <div className="bg-jurreyellow-dark flex flex-col">
+      <div className="flex justify-between px-16 py-8">
+        <Logo color="text-white" />
+        <div className="bg-white rounded-[20px]">
+          <p className="font-euclid font-medium">Let&apos;s Talk!</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center text-[20rem] noselect">
+        <p className="text-jurreyellow-light font-freigeistwide font-black">
+          JURRE
+        </p>
+      </div>
     </div>
   )
 }
