1ff3a2dbe9ffb6880a369dd3d07cc99c981861bc
Jurredr <github@jurre.me>
2021-10-29 10:25:18 +0200

    Added color prop to Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Logo() {
+interface Props {
+  color: string
+}
+
+const Logo: React.FC<Props> = (props) => {
   return (
-    <div className="footer-logo flex items-baseline justify-center gap-2 cursor-default">
+    <div className="footer-logo flex items-baseline justify-center gap-2 cursor-default noselect">
       <Image
+        className={props.color}
         src="/icons/logo-icon.svg"
         alt=""
         width={27}
         height={28}
         draggable="false"
       />
-      <p className="text-black font-medium text-3xl">Jurre.me</p>
+      <p className={props.color + ' font-medium text-3xl'}>Jurre.me</p>
     </div>
   )
 }
+
+export default Logo
