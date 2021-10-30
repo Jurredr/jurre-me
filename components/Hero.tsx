dc7cc71fef23d784801030f4db8645df6370f12d
Jurredr <github@jurre.me>
2021-10-30 20:08:11 +0200

    Added motion animation on button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import Logo from './Logo'
 
@@ -6,9 +7,13 @@ export default function Hero() {
     <div className="bg-jurreyellow-dark flex flex-col">
       <div className="flex justify-between px-16 py-8">
         <Logo color="white" />
-        <div className="bg-white rounded-[20px] cursor-pointer py-3 px-8">
-          <p className="font-euclid font-medium">Let&apos;s Talk!</p>
-        </div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 1.0 }}
+          className="bg-white rounded-[20px] cursor-pointer py-3 px-8"
+        >
+          <p className="font-euclid font-medium noselect">Let&apos;s Talk!</p>
+        </motion.div>
       </div>
       <div className="flex justify-center items-center noselect">
         <p className="text-jurreyellow-light font-freigeistwide font-black pb-72 text-[19vw] tracking-tight">
