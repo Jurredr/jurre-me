f98db149e9adb492fd0969b167272e818f8fe51b
Jurredr <github@jurre.me>
2021-10-31 19:24:53 +0100

    Added About and Work nav links + more padding on Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,16 +7,20 @@ export default function Hero() {
     <div className="bg-jurreyellow-dark flex flex-col">
       <div className="flex justify-between px-16 py-8">
         <Logo color="white" />
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1.0 }}
-          className="bg-white rounded-[20px] cursor-pointer py-3 px-8"
-        >
-          <p className="font-euclid font-medium noselect">Let&apos;s Talk!</p>
-        </motion.div>
+        <div className="flex justify-center items-center gap-8">
+          <p className="text-white cursor-pointer font-medium">About</p>
+          <p className="text-white cursor-pointer font-medium">Work</p>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.05 }}
+            className="bg-white rounded-[20px] cursor-pointer py-3 px-8"
+          >
+            <p className="font-euclid font-medium noselect">Let&apos;s Talk!</p>
+          </motion.div>
+        </div>
       </div>
-      <div className="flex justify-center items-center noselect">
-        <p className="text-jurreyellow-light font-freigeistwide font-black pb-72 text-[19vw] tracking-tight">
+      <div className="flex justify-center items-center noselect pt-12">
+        <p className="text-jurreyellow-light font-freigeistwide font-black pb-92 text-[19vw] tracking-tight">
           JURRE
         </p>
       </div>
