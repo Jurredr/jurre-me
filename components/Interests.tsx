b108b20d666f99cf81d5a1d5d61b8ee252fa4c0a
Jurredr <github@jurre.me>
2021-11-02 15:52:44 +0100

    Vertical text now works with Windi

--- components/Interests.tsx
+++ components/Interests.tsx
@@ -3,30 +3,18 @@ import React from 'react'
 const Interests: React.FC = () => {
   return (
     <div className="flex justify-center items-center font-freigeistwide font-black noselect text-[12vw] py-24">
-      <p
-        className="text-jurreyellow"
-        style={{ writingMode: 'vertical-lr', textOrientation: 'upright' }}
-      >
+      <p className="text-jurreyellow write-vertical-left write-orient-upright">
         CODE
       </p>
       <div className="flex flex-col">
-        <p
-          className="text-jurrepurple"
-          style={{ writingMode: 'vertical-lr', textOrientation: 'upright' }}
-        >
+        <p className="text-jurrepurple write-vertical-left write-orient-upright">
           UI
         </p>
-        <p
-          className="text-jurreblue"
-          style={{ writingMode: 'vertical-lr', textOrientation: 'upright' }}
-        >
+        <p className="text-jurreblue write-vertical-left write-orient-upright">
           UX
         </p>
       </div>
-      <p
-        className="text-jurremagenta write-vertical-left write-orient-upright"
-        // style={{ writingMode: 'vertical-lr', textOrientation: 'upright' }}
-      >
+      <p className="text-jurremagenta write-vertical-left write-orient-upright">
         BIZZ
       </p>
     </div>
