e8687da32a0ade312e8b2f5c83f9dcee147a630c
Jurredr <github@jurre.me>
2021-11-02 15:40:38 +0100

    Added custom cursor click animation

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -5,11 +5,24 @@ const Cursor: React.FC = () => {
   const innerCursorRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    return window.addEventListener('mousemove', (e) => {
+    // Mouse move
+    window.addEventListener('mousemove', (e) => {
       if (cursorRef.current && innerCursorRef.current) {
         cursorRef.current.style.cssText =
           innerCursorRef.current.style.cssText = `left: ${e.clientX}px; top: ${e.clientY}px;`
       }
+    })
+
+    // Mouse down
+    window.addEventListener('mousedown', (e) => {
+      if (innerCursorRef.current)
+        innerCursorRef.current.style.cssText = `left: ${e.clientX}px; top: ${e.clientY}px; width: 1rem; height: 1rem;`
+    })
+
+    // Mouse up
+    window.addEventListener('mouseup', (e) => {
+      if (innerCursorRef.current)
+        innerCursorRef.current.style.cssText = `left: ${e.clientX}px; top: ${e.clientY}px; width: 0.5rem; height: 0.5rem;`
     })
   }, [])
 
@@ -23,9 +36,7 @@ const Cursor: React.FC = () => {
       />
       <div
         ref={innerCursorRef}
-        className={
-          'fixed w-2 h-2 z-10 bg-black rounded-full pointer-events-none duration-100 transform -translate-x-1/2 -translate-y-1/2'
-        }
+        className="fixed w-2 h-2 z-10 bg-black rounded-full pointer-events-none duration-100 transform -translate-x-1/2 -translate-y-1/2"
       />
     </div>
   )
