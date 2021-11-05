5ed0577b0d2f501e8b3de528ab5f59c345ebd1f8
Jurredr <github@jurre.me>
2021-11-05 23:58:19 +0100

    Added scroll listener on Interests section

--- components/Interests.tsx
+++ components/Interests.tsx
@@ -1,6 +1,10 @@
+import { useTransform, useViewportScroll } from 'framer-motion'
 import React from 'react'
 
 const Interests: React.FC = () => {
+  const { scrollYProgress } = useViewportScroll()
+  const scaledY = useTransform(scrollYProgress, [0, 1], [1, 100])
+
   return (
     <div className="flex justify-center items-center font-freigeistwide font-black noselect text-[12vw] my-[14.6rem]">
       <p className="text-jurreyellow write-vertical-left write-orient-upright">
