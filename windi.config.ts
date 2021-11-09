986f424c3c4af2a4350d69f11b956588d6330095
Jurredr <github@jurre.me>
2021-11-09 12:35:48 +0100

    Fixed header nav responsivity on Galaxy Fold (added XS size)

--- windi.config.ts
+++ windi.config.ts
@@ -28,7 +28,12 @@ export default defineConfig({
       }
     },
     screens: {
-      xs: '320px'
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
     }
   },
   plugins: []
