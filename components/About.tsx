f2d57dcf81b8abd0e07dafd6b09d617d10d4cd9b
Jurredr <github@jurre.me>
2021-11-18 16:49:22 +0100

    About section now uses GSAP and ScrollMagic

--- components/About.tsx
+++ components/About.tsx
@@ -16,6 +16,18 @@ const About: React.FC = () => {
     triggerOnce: true
   })
 
+  const { scrollYProgress } = useViewportScroll()
+  const scaledY = useTransform(
+    scrollYProgress,
+    [0, 0.205, 0.37, 0.5],
+    [0.7, 1.0, 1.0, 0.9]
+  )
+  const borderY = useTransform(
+    scrollYProgress,
+    [0, 0.205, 0.22, 0.35, 0.37, 0.5],
+    [40, 40, 0, 0, 40, 40]
+  )
+
   useEffect(() => {
     if (introInView) {
       introControls.start('visible')
@@ -42,11 +54,16 @@ const About: React.FC = () => {
           textSmall="Welcome to my world. Get to know me :)"
         />
       </motion.div>
-      <Controller>
-        <Scene triggerHook="onLeave" duration={1000} pin>
-          <div className="w-[89%] h-screen mt-25 bg-jurremagenta rounded-[40px]"></div>
-        </Scene>
-      </Controller>
+      <div className="w-screen">
+        <Controller>
+          <Scene triggerHook="onLeave" duration={1000} pin>
+            <motion.div
+              className="w-screen h-screen mt-25 bg-jurremagenta"
+              style={{ scaleX: scaledY, borderRadius: borderY }}
+            ></motion.div>
+          </Scene>
+        </Controller>
+      </div>
     </div>
   )
 }
