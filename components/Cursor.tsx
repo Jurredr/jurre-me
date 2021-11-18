80c2deff8599e29b689bdee1cb5131d0dd952e98
Jurredr <github@jurre.me>
2021-11-18 18:07:03 +0100

    Removed unused import

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Cursor: React.FC = () => {
   const cursorRef = useRef<HTMLDivElement>(null)
