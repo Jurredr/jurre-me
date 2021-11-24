a7b3077f73cc85555b3f089640569ddb916e9e14
Jurredr <github@jurre.me>
2021-11-24 11:39:11 +0100

    Added props for dynamic inputs

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -1,16 +1,24 @@
 import LanguageFlair from './LanguageFlair'
 
-const WorkCard: React.FC = () => {
+interface Props {
+  name: string
+  title: string
+  languages: string[][]
+}
+
+const WorkCard: React.FC<Props> = (props) => {
   return (
     <div className="flex flex-col text-white gap-7">
-      <p className="font-euclid font-medium text-2xl">Cashew</p>
+      <p className="font-euclid font-medium text-2xl">{props.name}</p>
       <div className="font-freigeist font-bold text-5xl flex flex-col gap-4">
-        <p>Personal finance platform</p>
-        <p>for budgeting and overview</p>
+        {props.title.split('[split]').map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
       </div>
       <div className="flex font-euclid text-lg gap-2 mt-4">
-        <LanguageFlair name="JavaScript" color="orange" />
-        <LanguageFlair name="Vue" color="teal" />
+        {props.languages.map((item, index) => {
+          return <LanguageFlair key={index} name={item[0]} color={item[1]} />
+        })}
       </div>
     </div>
   )
