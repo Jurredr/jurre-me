a7e58f44b37d712d02e6a6c0a027f2f5bb6e5cb2
Jurredr <github@jurre.me>
2022-03-02 00:08:18 +0100

    Fixed age calculator

--- lib/Age.ts
+++ lib/Age.ts
@@ -1,6 +1,6 @@
 const getMyCurrentAge = () => {
   const today = new Date()
-  const birthDate = new Date('29-07-2002')
+  const birthDate = new Date('07-29-2002')
   let age = today.getFullYear() - birthDate.getFullYear()
   const m = today.getMonth() - birthDate.getMonth()
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
