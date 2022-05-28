2444afd0253be83e5622120d3081c8f36120f82e
Jurredr <github@jurre.me>
2022-05-28 13:59:35 +0200

    Added in custom fonts + custom styling & favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        freigeist: ['Freigeist'],
+        freigeistwide: ['Freigeist Wide'],
+        euclid: ['Euclid Circular B']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
