c2ffa3e0e143d03004c941b2dda03a9928b811ac
Jurredr <github@jurre.me>
2022-05-29 22:19:16 +0200

    Added all custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
         freigeist: ['Freigeist'],
         freigeistwide: ['Freigeist Wide'],
         euclid: ['Euclid Circular B']
+      },
+      colors: {
+        jurreyellow: '#FFC400',
+        jurrelightyellow: '#FFC35E',
+        jurrelightpurple: '#DC98FF',
+        jurrelightblue: '#54BBFF',
+        jurredarkyellow: '#FFAD00',
+        jurredarkpurple: '#B729FF',
+        jurredarkblue: '#00A8FF',
+        jurregray: '#F0F1F4',
+        jurreoffwhite: '#F5F0F5',
+        jurredark: '#1C1C27',
+        jurredarklight: '#28293E',
+        jurredarklighter: '#42426A'
       }
     }
   },
