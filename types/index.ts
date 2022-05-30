51a69f9bf78e4a69b18cbab13f7a7a6238bc40d2
Jurredr <github@jurre.me>
2022-05-30 17:30:46 +0200

    Updated Author type for post authors

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,16 @@
 export type Route = { label: string; path: string }
+export type Author = {
+  name: string
+  avatarUrl: string
+  meta: {
+    instagram: string
+    github: string
+    twitter: string
+    discord: string
+    linkedin: string
+    last_name: string
+    first_name: string
+    website: string
+    description: string
+  }
+}
