fdd956bc9bbe45e0964f6125725855ce52d59bb4
Jurredr <github@jurre.me>
2022-05-30 17:31:39 +0200

    Added About and Projects page stubs

---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import { Layout } from '../components/Layout'
+import { SEO } from '../components/SEO'
+
+const About: NextPage = () => {
+  const title = 'About'
+  const description = 'Want to know more about Jurre de Ruiter? Get to know me!'
+
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        description={description}
+        imageUrl={'/img/about.png'}
+      />
+    </Layout>
+  )
+}
+
+export default About
---
+++ pages/projects/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import { Layout } from '../../components/Layout'
+import { SEO } from '../../components/SEO'
+
+const Projects: NextPage = () => {
+  const title = 'Projects'
+  const description =
+    'Find out about all the development projects I have worked on!'
+
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        description={description}
+        imageUrl={'/img/about.png'}
+      />
+    </Layout>
+  )
+}
+
+export default Projects
