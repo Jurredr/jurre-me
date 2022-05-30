760ebbbc5126ff31b729a6551d8fabaeae2debc2
Jurredr <github@jurre.me>
2022-05-30 17:40:01 +0200

    Added custom SEO handling through NextSeo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import WebHead from '../components/WebHead'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
+import { SEO } from '../components/SEO'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,8 +13,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
         />
       </Head>
-      {/* The Head metadata */}
-      <WebHead />
+      {/* The default SEO configuration */}
+      <SEO title="Home" />
       <ThemeProvider attribute="class">
         {/* The page component */}
         <Component {...pageProps} />
