799bd99afef94b0e6c7d97afb301f0ba9a06d520
Jurredr <github@jurre.me>
2022-05-30 13:23:53 +0200

    Added home tools scroll divider section

---
+++ components/home/HomeToolScroller.tsx
@@ -0,0 +1,7 @@
+const HomeToolScroller: React.FC = () => {
+  return (
+    <div className="h-44 w-full bg-jurrelightpurple dark:bg-jurredarkpurple"></div>
+  )
+}
+
+export default HomeToolScroller
