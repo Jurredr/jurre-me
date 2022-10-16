3f3171ba82de61406bd38555dfa869c07c6d2e29
Jurredr <github@jurre.me>
2022-10-17 00:13:45 +0100

    Added Google Analytics to app root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,39 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 import { SEO } from '../components/common/SEO'
 import Cursor from '../components/common/Cursor'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
+      {/* Viewport */}
       <Head>
         <meta
           name="viewport"
           content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
         />
       </Head>
+
       {/* The default SEO configuration */}
       <SEO title="Home" />
+
+      {/* The base site and its theme provider */}
       <ThemeProvider attribute="class">
         {/* The custom cursor */}
         <Cursor />
